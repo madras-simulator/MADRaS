ddd505fee2b45df576fa2013e13695eaa994fe47
rudrasohan <sohanrudra@gmail.com>
2018-09-04 22:40:57 +0530

    created starting point

---
+++ scripts/autostart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+xte 'key Return'
+xte 'usleep 100000'
+xte 'key Return'
+
+# Uncomment following four lines for practice session
+# xte 'usleep 100000'
+# xte 'key Up'
+# xte 'usleep 100000'
+# xte 'key Up'
+
+xte 'usleep 100000'
+xte 'key Return'
+xte 'usleep 100000'
+xte 'key Return'
+xte 'usleep 100000'
+xte 'key Return'
+# Uncomment following four lines to increase speed of TORCS simulator by 4x
+#xte 'usleep 100000'
+#xte 'keydown Shift_L'; xte 'key plus'; xte 'keyup Shift_L'
+#xte 'usleep 100000'
+#xte 'keydown Shift_L'; xte 'key plus'; xte 'keyup Shift_L'
