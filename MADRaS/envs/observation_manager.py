Observation normalization fixed

MADRaS/envs/observation_manager.py
@@ -11,6 +11,8 @@ class ObservationManager(object):
             raise ValueError("Unrecognized observation mode {}".format(mode))
 
     def get_obs(self, full_obs, game_config):
+        if self.cfg["normalize"]:
+            full_obs = self.normalize_obs(full_obs, game_config)
         return self.obs.get_obs(full_obs)
 
     def min_max_normalize(self, var, min, max):
@@ -19,23 +21,10 @@ class ObservationManager(object):
         return (var - offset) / scale
 
     def normalize_obs(self, full_obs, game_config):
-        # convert speeds to m/s
-        full_obs.speedX = full_obs.speedX*1000.0/3600.0
-        full_obs.speedY = full_obs.speedY*1000.0/3600.0
-        full_obs.speedZ = full_obs.speedZ*1000.0/3600.0
-
-        # normalize
-        self.cfg["obs_min"]["speedX"] = - game_config.target_speed
-        self.cfg["obs_min"]["speedY"] = - game_config.target_speed
-        self.cfg["obs_min"]["speedZ"] = - game_config.target_speed
-        self.cfg["obs_max"]["speedX"] = game_config.target_speed
-        self.cfg["obs_max"]["speedY"] = game_config.target_speed
-        self.cfg["obs_max"]["speedZ"] = game_config.target_speed
-
         for key in self.cfg["obs_min"]:
             exec("full_obs.{} = self.min_max_normalize(full_obs.{}, self.cfg['obs_min'][{}], self.cfg['obs_max'][{}])".format(
                 key, key, key, key))
-
+        return full_obs
 
 
 class MadrasObs(object):
@@ -60,4 +49,14 @@ class TorcsObs(MadrasObs):
                         full_obs.speedZ,
                         full_obs.wheelSpinVel / 100.0,
                         full_obs.rpm))
+        return obs
+
+class SingleAgentSimpleLapObs(MadrasObs):
+    def get_obs(self, full_obs):
+        obs = np.hstack((full_obs.angle,
+                        full_obs.track,
+                        full_obs.trackPos,
+                        full_obs.speedX,
+                        full_obs.speedY,
+                        full_obs.speedZ))
         return obs

MADRaS/utils/gym_torcs.py
@@ -265,9 +265,9 @@ class TorcsEnv:
                      'wheelSpinVel']
             Observation = col.namedtuple('Observaion', names)
             return Observation(focus=np.array(raw_obs['focus'], dtype=madras.floatX)/200.,
-                               speedX=np.array(raw_obs['speedX'], dtype=madras.floatX)/300.0,
-                               speedY=np.array(raw_obs['speedY'], dtype=madras.floatX)/300.0,
-                               speedZ=np.array(raw_obs['speedZ'], dtype=madras.floatX)/300.0,
+                               speedX=np.array(raw_obs['speedX'], dtype=madras.floatX)/self.default_speed,
+                               speedY=np.array(raw_obs['speedY'], dtype=madras.floatX)/self.default_speed,
+                               speedZ=np.array(raw_obs['speedZ'], dtype=madras.floatX)/self.default_speed,
                                angle=np.array(raw_obs['angle'], dtype=madras.floatX)/3.1416,
                                damage=np.array(raw_obs['damage'], dtype=madras.floatX),
                                opponents=np.array(raw_obs['opponents'], dtype=madras.floatX)/200.,