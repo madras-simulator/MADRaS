dbd27b78012a129eee232f6c201f8ab678d93e1c
santara <nrbnsntr@gmail.com>
2019-12-05 13:32:44 +0530

    Curriculum learning experiments under way

--- MADRaS/eval_rllib_agent.py
+++ MADRaS/eval_rllib_agent.py
@@ -22,6 +22,10 @@ gym.envs.register(
 import os
 
 
-CHECKPOINT = "/home/anirban/ray_results/PPO_madras_env_2019-12-03_10-11-12ek55po4b/checkpoint_1734/checkpoint-1734"
+# CHECKPOINT = "/home/anirban/ray_results/PPO_madras_env_2019-12-03_20-16-31w9ik1q41/checkpoint_1052/checkpoint-1052"
+# Beautiful checkpoint for Spring:
+# CHECKPOINT = "/home/anirban/ray_results/PPO_madras_env_2019-12-04_14-27-53f19t1utp/checkpoint_1893/checkpoint-1893"
+CHECKPOINT = "/home/anirban/ray_results/PPO_madras_env_2019-12-04_20-03-37u4x3v1qw/checkpoint_61/checkpoint-61"
+CHECKPOINT = "/home/anirban/ray_results/PPO_madras_env_2019-12-05_07-11-40l711dsji/checkpoint_31/checkpoint-31"
 os.system("rllib rollout {} --env Madras-v0 --steps 1000000 --run PPO --no-render".format(CHECKPOINT))
 os.system("pkill torcs")
--- MADRaS/train_rllib_agent.py
+++ MADRaS/train_rllib_agent.py
@@ -12,14 +12,23 @@ config["num_gpus"] = 0
 config["num_workers"] = 1
 config["eager"] = False
 config["vf_clip_param"] = 20  # originally it was 10. We should consider scaling down the rewards for keeping episode reward under 2000
-config["gamma"] = 0.7
+# config["gamma"] = 0.7
+# config["lr"] = 5e-7
+# config["batch_mode"] = "complete_episodes"
+# config["train_batch_size"] = 10000
 
 trainer = ppo.PPOTrainer(config=config, env="madras_env")
 
 # Can optionally call trainer.restore(path) to load a checkpoint.
 # trainer.restore('/home/anirban/ray_results/PPO_madras_env_2019-12-03_08-56-11fe8guncf/checkpoint_1713/checkpoint-1713')
-trainer.restore('/home/anirban/ray_results/PPO_madras_env_2019-12-02_13-56-27aqa7ang9/checkpoint_1262/checkpoint-1262')
+# trainer.restore('/home/anirban/ray_results/PPO_madras_env_2019-12-03_19-15-01h5u9w2bj/checkpoint_311/checkpoint-311')
 
+# Alpine checkpoint from which Spring was restored:
+# trainer.restore('/home/anirban/ray_results/PPO_madras_env_2019-12-04_06-36-12t0juqqsq/checkpoint_471/checkpoint-471')
+
+# Good Spring checkpoint
+# trainer.restore('/home/anirban/ray_results/PPO_madras_env_2019-12-04_09-37-1648nnevup/checkpoint_1802/checkpoint-1802')
+# Trying to solve the issue of crash at 15813-15817 step range which never seems to appear during training
 for i in range(10000):
    # Perform one iteration of training the policy with PPO
    result = trainer.train()
