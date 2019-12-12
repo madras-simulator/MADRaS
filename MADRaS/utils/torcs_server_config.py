ebf929da79e041cbdb24172c56e4120845d8edd4
santara <nrbnsntr@gmail.com>
2019-12-12 18:39:49 +0530

    Traffic cars look different from learning car

--- MADRaS/utils/torcs_server_config.py
+++ MADRaS/utils/torcs_server_config.py
@@ -4,6 +4,13 @@ import os
 import logging
 logger = logging.getLogger(__name__)
 
+path_and_file = os.path.realpath(__file__)
+path, _ = os.path.split(path_and_file)
+
+QUICKRACE_TEMPLATE_PATH = os.path.join(path, "data", "quickrace.template")
+CAR_CONFIG_TEMPATE_PATH = os.path.join(path, "data", "car_config.template")
+SCR_SERVER_CONFIG_TEMPLATE_PATH = os.path.join(path, "data", "scr_server_config.template")
+
 MAX_NUM_CARS = 10  # There are 10 scr-servers in the TORCS GUI
 TRACK_NAMES = [
     "aalborg",
@@ -29,91 +36,6 @@ TRACK_NAMES = [
     "wheel-2"
 ]
 
-QUICKRACE_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE params SYSTEM "params.dtd">
-
-
-<params name="Quick Race">
-  <section name="Header">
-    <attstr name="name" val="Quick Race"/>
-    <attstr name="description" val="Quick Race"/>
-    <attnum name="priority" val="10"/>
-    <attstr name="menu image" val="data/img/splash-qr.png"/>
-  </section>
-
-  <section name="Tracks">
-    <attnum name="maximum number" val="1"/>
-    <section name="1">
-      <attstr name="name" val="{track_name}"/>
-      <attstr name="category" val="{track_category}"/>
-    </section>
-
-  </section>
-
-  <section name="Races">
-    <section name="1">
-      <attstr name="name" val="Quick Race"/>
-    </section>
-
-  </section>
-
-  <section name="Quick Race">
-    <attnum name="distance" unit="km" val="0"/>
-    <attstr name="type" val="race"/>
-    <attstr name="starting order" val="drivers list"/>
-    <attstr name="restart" val="yes"/>
-    <attnum name="laps" val="1"/>
-    <section name="Starting Grid">
-      <attnum name="rows" val="2"/>
-      <attnum name="distance to start" val="{distance_to_start}"/>
-      <attnum name="distance between columns" val="20"/>
-      <attnum name="offset within a column" val="10"/>
-      <attnum name="initial speed" val="0"/>
-      <attnum name="initial height" val="0.2"/>
-    </section>
-
-  </section>
-
-  <section name="Drivers">
-    <attnum name="maximum number" val="40"/>
-    <attnum name="focused idx" val="0"/>
-    <attstr name="focused module" val="scr_server"/>
-    {car_config}
-
-  </section>
-
-  <section name="Configuration">
-    <attnum name="current configuration" val="4"/>
-    <section name="1">
-      <attstr name="type" val="track select"/>
-    </section>
-
-    <section name="2">
-      <attstr name="type" val="drivers select"/>
-    </section>
-
-    <section name="3">
-      <attstr name="type" val="race config"/>
-      <attstr name="race" val="Quick Race"/>
-      <section name="Options">
-        <section name="1">
-          <attstr name="type" val="race length"/>
-        </section>
-
-      </section>
-
-    </section>
-
-  </section>
-
-</params>
-"""
-
-CAR_CONFIG_TEMPATE = """<section name="{section_no}">
-      <attnum name="idx" val="{car_no}"/>
-      <attstr name="module" val="scr_server"/>
-    </section>
-"""
 
 class TorcsConfig(object):
     def __init__(self, cfg, randomize=False):
@@ -121,12 +43,21 @@ class TorcsConfig(object):
         self.track_category = "road"  # for test purposes - will add "dirt" later
         self.track_names = cfg["track_names"] if  "track_names" in cfg else TRACK_NAMES
         self.distance_to_start = cfg["distance_to_start"] if "distance_to_start" in cfg else 0
-        self.torcs_server_config_path = cfg["torcs_server_config_path"] if "torcs_server_config_path" in cfg else "/tmp"
-        self.template = QUICKRACE_TEMPLATE
-        self.car_config_template = CAR_CONFIG_TEMPATE
-        self.randomize = randomize
-        os.system("mkdir -p {}".format(self.torcs_server_config_path))
-        self.quickrace_path = os.path.join(self.torcs_server_config_path, "quickrace.xml")
+        self.torcs_server_config_dir = (cfg["torcs_server_config_dir"] if "torcs_server_config_dir" in cfg
+                                        else "/home/anirban/.torcs/config/raceman/")
+        self.scr_server_config_dir = (cfg["scr_server_config_dir"] if "scr_server_config_dir" in cfg
+                                          else "/home/anirban/usr/local/share/games/torcs/drivers/scr_server/")
+        with open(QUICKRACE_TEMPLATE_PATH, 'r') as f:
+            self.quickrace_template = f.read()
+        with open(CAR_CONFIG_TEMPATE_PATH, 'r') as f:
+            self.car_config_template = f.read()
+        with open(SCR_SERVER_CONFIG_TEMPLATE_PATH, 'r') as f:
+            self.scr_server_config_template = f.read()
+        self.randomize = randomize)
+        self.quickrace_xml_path = os.path.join(self.torcs_server_config_dir, "quickrace.xml")
+        self.scr_server_xml_path = os.path.join(self.scr_server_config_dir, "scr_server.xml")
+        self.traffic_car_type = cfg['traffic_car']
+        self.learning_car_type = cfg['learning_car']
 
     def get_num_traffic_cars(self):
         if not self.randomize:
@@ -147,15 +78,21 @@ class TorcsConfig(object):
         self.num_traffic_cars = self.get_num_traffic_cars()
         self.num_learning_cars = 1
 
-        car_config = "\n    ".join(self.car_config_template.format(
-                               **{"section_no": i+1, "car_no": i}) for i in range(
-                                  self.num_traffic_cars + self.num_learning_cars))
+        car_config = "\n".join(self.car_config_template.format(
+                      **{"section_no": i+1, "car_no": i}) for i in range(
+                        self.num_traffic_cars + self.num_learning_cars))
         context = {
             "track_name": self.get_track_name(),
             "track_category": self.track_category,
             "distance_to_start": self.distance_to_start,
             "car_config": car_config
         }
-        torcs_server_config = self.template.format(**context)
-        with open(self.quickrace_path, "w") as f:
+        torcs_server_config = self.quickrace_template.format(**context)
+        with open(self.quickrace_xml_path, "w") as f:
             f.write(torcs_server_config)
+
+        car_name_list = ([self.traffic_car_type]*self.num_traffic_cars + [self.learning_car_type] +
+                         [self.traffic_car_type]*(MAX_NUM_CARS-self.num_traffic_cars-1))
+        scr_server_config = self.scr_server_config_template.format(*car_name_list)
+        with open(self.scr_server_xml_path, "w") as f:
+            f.write(scr_server_config)
